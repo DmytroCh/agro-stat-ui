3ac377c86ccd6e4c7f7164473938ac67cc742b85
DmytroCh <dimabest92@gmail.com>
2021-01-30 22:29:17 +0100

    remove not used files

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import './css/index.scss';
 import './css/loading.scss';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './components/LoadingScreen';
 import LoadingScreen from './components/LoadingScreen';
 
@@ -23,7 +22,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

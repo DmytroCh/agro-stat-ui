f496e2ac2c0e70c7af0fd470d87524f0e55111f8
DmytroCh <dimabest92@gmail.com>
2021-03-04 23:16:05 +0100

    Styling done. Filtering logic todo

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,8 +7,10 @@ import { SemanticDatepickerProps } from 'react-semantic-ui-datepickers/dist/type
 import { Header } from 'semantic-ui-react';
 import { Crop } from '../../Model/types';
 import { TFunction } from 'i18next';
+import SideBarButton from './SideBarButton';
 
 interface State {
+    isVisible: true | false
 }
 
 interface Props {
@@ -17,20 +19,35 @@ interface Props {
 }
 
 export default class Content extends Component<Props, State> {
+    state = {
+        isVisible: false
+    }
 
     setDatesRange(event: React.SyntheticEvent | undefined, data: SemanticDatepickerProps) {
         console.log(data);
     }
 
+    setVisibility = ():void => {
+        this.setState((prevState) => {
+            return {
+                isVisible: !prevState.isVisible
+            }
+        });
+    }
+
     render() {
         return (
             <div className='side-bar'>
-                <Header as='h1'>{this.props.i18n(this.props.cropName)}</Header>
-                <SemanticDatepicker 
-                    onChange={this.setDatesRange}
-                    datePickerOnly={ true }
-                    type='range'
-                />
+                <div className={`filters ${this.state.isVisible ? "" : "hidden"}`}>
+                    <Header as='h1'>{this.props.i18n(this.props.cropName)}</Header>
+                    <Header as='h3'>{this.props.i18n('data_range_label')}</Header>
+                    <SemanticDatepicker 
+                        onChange={this.setDatesRange}
+                        datePickerOnly={ true }
+                        type='range'
+                    />
+                </div>
+                <SideBarButton isVisible={this.state.isVisible} onClick={this.setVisibility}></SideBarButton>
             </div>
         )
     }
